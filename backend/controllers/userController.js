e98b8008bb598855c8bb6d6d62eca4e399404be6
ViniUK00 <elvin.bilo@icloud.com>
2023-03-15 01:45:22 +0000

    Error & Exception Handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,23 @@
+const axios = require("axios")
+
+// @desc Register a new user
+// @route /api/users
+// @access Public
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error("Please include all fields")
+  }
+
   res.send("Register Route")
 }
 
+// @desc Login a user
+// @route /api/login
+// @access Public
 const loginUser = (req, res) => {
   res.send("Login Route")
 }
